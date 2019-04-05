602d59700fa56e224dbe27a784d2110c2bcdb0c3
Gerardo Keys <jotojry@icloud.com>
2019-04-04 17:53:28 -0700

    at work commit 04/04

--- js/conf.js
+++ js/conf.js
@@ -10,29 +10,29 @@ title.on('change', (e) => {
    };
 });
 
+// Put this in main function. This hides the other-title input upon load.
 $('#other-title').css("display", "none");
 
-// if js puns
-    // hide last three
-// else if js love
-    // hide top three
-
-
+// This is the event listener for the design select element. Depending on selection
+// color will change accordingly.
 design.on('change', (e) => {
     if(e.target.value === 'js puns'){
-        $('#color option[selected="selected"]').removeAttr('selected')
-        color[0][0].setAttribute('selected', 'selected');
-        for (let i = 0; i < color[0].length; i ++){
-            i <= 2 ? color[0][i].style.display = '' : color[0][i].style.display = 'none';
-        }
+        $('#color option[selected="selected"]').removeAttr('selected');
+        $('#color option[value="cornflowerblue"]').attr('selected', 'selected');
+        $('#color option').each(function(i){
+            i <= 2 ? this.style.display = '' : this.style.display = 'none';
+        })
     } else if(e.target.value === 'heart js'){
         $('#color option[value="tomato"]').attr('selected', 'selected');
         for (let i = 0; i < color[0].length; i ++){
             i >= 3 ? color[0][i].style.display = '' : color[0][i].style.display = 'none';
         }
+    } else if(e.target.value === 'Select Theme') {
+        for(let i = 0; i < color[0].length; i ++){
+            color[0][i].style.display = '';
+        }
     }
     
-    console.log(e.target.value)
 })
 
 
